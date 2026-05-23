4bd7e627d474895cc2d888eed745278fcfcecafe
Gabriel Souza <gbr_souza06@outlook.com>
2026-05-22 23:04:06 -0300

    feat(p2): chama a api e mostra parte do resultado

--- ConsultaPaises/App.js
+++ ConsultaPaises/App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const [res, setRes] = useState(null);
+
+  useEffect(() => {
+    async function buscarPais() {
+      const resposta = await fetch(
+        "https://restcountries.com/v3.1/name/brazil"
+      );
+      const dados = await resposta.json();
+      setRes(dados[0]);
+    }
+
+    buscarPais();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {res && (
+        <Text style={{ fontSize: 20 }}>
+          {res.name.common}
+        </Text>
+      )}
+      {res && (
+        <Text style={{ fontSize: 20 }}>
+          {res.name.nativeName.por.official}
+        </Text>
+      )}
+      {res && (
+        <Text style={{ fontSize: 20 }}>
+          {res.translations.rus.common}
+        </Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
