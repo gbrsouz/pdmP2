0a4dd2b71f823a27c17182c0912c0eae22078a43
Gabriel Souza <gbr_souza06@outlook.com>
2026-05-27 18:48:33 -0300

    refactor(p2): muda a pesquisa para usar picker

--- ConsultaPaises/App.js
+++ ConsultaPaises/App.js
@@ -1,68 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Pressable, Image} from 'react-native';
 import { useEffect, useState } from "react";
+import { Picker } from '@react-native-picker/picker';
 
 export default function App() {
   const [res, setRes] = useState(null);
-  const [nomePais, setNomePais] = useState("");
-  const [nomeCapital, setNomeCapital] = useState("");
+  const [tipoBusca, setTipoBusca] = useState("pais");
+  const [entrada, setEntrada] = useState("");
 
-  async function buscarPais() {
-    const resposta = await fetch( 
-      `https://restcountries.com/v3.1/name/${nomePais}`
-    );
+  async function buscar() {
+    const url =
+      tipoBusca === "pais"
+        ? `https://restcountries.com/v3.1/name/${entrada}`
+        : `https://restcountries.com/v3.1/capital/${entrada}`;
+
+    const resposta = await fetch(url);
     const dados = await resposta.json();
     setRes(dados[0]);
   }
 
-  async function buscarCapital() {
-    const resposta = await fetch( 
-      `https://restcountries.com/v3.1/capital/${nomeCapital}`
-    );
-    const dadosCap = await resposta.json();
-    setRes(dadosCap[0]);
-  }
-
   return (
     <View style={styles.container}>
-        <Text>Pesquisa por País:</Text>
-        <TextInput
-          placeholder='Digite o nome de um País'
-          value={nomePais}
-          onChangeText={(novoTexto) => setNomePais(novoTexto)}
-          style={styles.input}
-        />
+      <Text style={{ marginBottom: 10 }}>Selecione o tipo de pesquisa:</Text>
+      <Picker
+        selectedValue={tipoBusca}
+        onValueChange={(valor) => setTipoBusca(valor)}
+        style={{ height: 30, width: 200, marginBottom: 30 }}
+      >
+        <Picker.Item label="Por País" value="pais" />
+        <Picker.Item label="Por Capital" value="capital" />
+      </Picker>
 
-        <Pressable style={styles.botao} onPress={buscarPais}>
-          <Text style={styles.textoBotao}>Pesquisar</Text>
-        </Pressable>
-        <Text>Pesquisa por Capital:</Text>
-        <TextInput
-          placeholder='Digite o nome de uma Capital'
-          value={nomeCapital}
-          onChangeText={(novoTextoCap) => setNomeCapital(novoTextoCap)}
-          style={styles.input}
-        />
+      <TextInput
+        placeholder={
+          tipoBusca === "pais"
+            ? "Digite o nome de um País"
+            : "Digite o nome de uma Capital"
+        }
+        value={entrada}
+        onChangeText={(novoTexto) => setEntrada(novoTexto)}
+        style={styles.input}
+      />
 
-        <Pressable style={styles.botao} onPress={buscarCapital}>
-          <Text style={styles.textoBotao}>Pesquisar</Text>
-        </Pressable>
+      <Pressable style={styles.botao} onPress={buscar}>
+        <Text style={styles.textoBotao}>Pesquisar</Text>
+      </Pressable>
 
-        {res && (
-          <View style={{ marginTop: 20 }}>
-            <Text style={{ fontSize: 20 }}>Nome Comum: {res.name.common}</Text>
+      {res && (
+        <View style={{ marginTop: 20, alignItems: 'center' }}>
+          <Text style={{ fontSize: 20 }}>Nome Comum: {res.name.common}</Text>
 
-            <Text style={{ fontSize: 20 }}>Nome Oficial: {res.name.official}</Text>
+          <Text style={{ fontSize: 20 }}>Nome Oficial: {res.name.official}</Text>
 
-            <Text style={{ fontSize: 20 }}>Nome - tradução para russo: {res.translations.rus.common}</Text>
-            
-            <Image
-              source={{ uri: res.flags.png }} 
-              style={{ width: 120, height: 80 }} 
-            />
-          </View>
-        )}
-        <StatusBar style="auto" />
+          <Text style={{ fontSize: 20 , marginBottom: 30 }}>Nome - tradução para russo: {res.translations.rus.common}</Text>
+
+          <Image
+            source={{ uri: res.flags.png }} 
+            style={{ width: 120, height: 80 }} 
+          />
+        </View>
+      )}
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -80,8 +78,8 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     paddingHorizontal: 10,
-    width: '100%',
-    marginBottom: 10,
+    width: '50%',
+    marginBottom: 30,
   },
   botao: {
     backgroundColor: '#007AFF',
